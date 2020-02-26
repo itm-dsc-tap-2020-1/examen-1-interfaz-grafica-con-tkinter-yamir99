dd3a6ff8a07159478540bfb9614435d33c028c1b
yamir99 <61289231+yamir99@users.noreply.github.com>
2020-02-26 11:58:48 -0600

    Add files via upload

--- examen_1.py
+++ examen_1.py
@@ -4,7 +4,15 @@ from tkinter import scrolledtext
 from tkinter import Menu
 from tkinter import messagebox as mBox
 def calificar ():
-    mBox.showinfo('Calificacion',"Tu calificaciones de: ")
+    calif =4
+    if opcion_1.get() ==1 or opcion_4.get() == 1:
+        calif = calif+2
+    if opcion.get() == 1:
+        calif = calif +2
+    if opcion_2.get() == 1:
+        calif = calif +2  
+    txt_final = (str) "Tu calificaciones de: "+calif
+    mBox.showinfo('Calificacion',txt_final)
     print ("hola")
 ventana = tk.Tk()
 ventana.geometry('+450+100')
@@ -20,23 +28,23 @@ caja_2 = scrolledtext.ScrolledText(ventana, width  = '45', height='3', wrap = tk
 caja_2.grid(column = 0,row = 3,columnspan=3,padx = 10)
 #Respuesta Nueva españa
 ttk.Label(ventana, text="3.- ¿Año en el que los españoles conquistaron tenochtitlan?").grid(pady = 10,padx = 10,column=0,row=4)
-opcion = tk.StringVar()
-radio1 = tk.Radiobutton(ventana,text = "1521", variable = opcion, value="1521",)
+opcion = tk.IntVar()
+radio1 = tk.Radiobutton(ventana,text = "1521", variable = opcion, value=1,)
 radio1.grid(pady =10,padx=10,column =0, row = 5, sticky = tk.W)
 radio1.select()
-radio2 = tk.Radiobutton(ventana,text = "2012", variable = opcion, value="2012")
+radio2 = tk.Radiobutton(ventana,text = "2012", variable = opcion, value=3)
 radio2.grid(pady =10,padx=10,column =1, row = 5, sticky = tk.W)
-radio3 = tk.Radiobutton(ventana,text = "1518", variable = opcion, value="1518")
+radio3 = tk.Radiobutton(ventana,text = "1518", variable = opcion, value=2)
 radio3.grid(pady =10,padx=10,column =2, row = 5, sticky = tk.W)
 
 ttk.Label(ventana, text="4.- ¿Nombre del conquistador que derroto al imperio mexica?").grid(pady = 10,padx = 10,column=0,row=6)
-opcion_2 = tk.StringVar()
-radio4 = tk.Radiobutton(ventana,text = "Cristobal Colon", variable = opcion_2, value="Cristobal",)
+opcion_2 = tk.IntVar()
+radio4 = tk.Radiobutton(ventana,text = "Cristobal Colon", variable = opcion_2, value=2,)
 radio4.grid(pady =10,padx=10,column =0, row = 7, sticky = tk.W)
 
-radio5 = tk.Radiobutton(ventana,text = "Hernán cortéz", variable = opcion_2, value="Hernan")
+radio5 = tk.Radiobutton(ventana,text = "Hernán cortéz", variable = opcion_2, value=1)
 radio5.grid(pady =10,padx=10,column =1, row = 7, sticky = tk.W)
-radio6 = tk.Radiobutton(ventana,text = "Guadalupe Victoria", variable = opcion_2, value="Guadalupe")
+radio6 = tk.Radiobutton(ventana,text = "Guadalupe Victoria", variable = opcion_2, value=3)
 radio6.grid(pady =10,padx=10,column =2, row = 7, sticky = tk.W)
 radio4.select()
 
